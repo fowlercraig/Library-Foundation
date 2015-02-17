78b27c23ec140a4b3f6af5a1ac6374a53e8131c6
Craig Fowler <craig@wearegiants.com>
2015-02-17 08:58:39 -0800

    Small Updates.

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@ require_once locate_template('/lib/cleanassnav.php' );
 include_once locate_template('/lib/soil-master/soil.php' );
 include_once locate_template('/lib/custom-post-types.php' );
 include_once locate_template('/lib/enque-js.php' );
-//include_once locate_template('/lib/woo-disablebilling.php' );
+include_once locate_template('/lib/woo-disablebilling.php' );
 include_once locate_template('/lib/json-api/json-api.php' );
 include_once locate_template('/lib/videoembed.php' );
 
